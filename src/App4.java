f79be63489157302e787b0bb9c818338426866a6
kit <venkata.amulya@gmail.com>
2017-06-13 09:53:07 -0500

    This is first commit

---
+++ src/App4.java
@@ -0,0 +1,23 @@
+import java.lang.*;
+  class App5 {
+	public void sum(double d,double e)
+	{
+		System.out.println(d+e);
+	}
+	public void sum(int a,int b)
+	{
+		System.out.println(a+b);
+	}
+  }
+	public class App4
+	{
+		public  static void main(String args[])
+		{
+		 App5 a=new App5();
+		 a.sum(2, 3);
+		a.sum(3.9, 6.0);
+	}
+	}
+	
+	
+
